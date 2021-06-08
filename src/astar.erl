9431f2090e83467b292e090fd1ec39b293c590fa
dong50252409 <304887750@qq.com>
2021-06-08 11:41:35 +0800

    完善 spec

--- src/astar.erl
+++ src/astar.erl
@@ -1,106 +1,117 @@
 -module(astar).
 
--export([heuristic_fun/1, neighbour_fun/3, search/5]).
+-export([search/4]).
 
 -define(MAX_LIMIT, 16#FFFF).
+-define(DISTANCE_FUNCTION, manhattan).
 
 -type grid() :: {integer(), integer()}.
--type direction() :: {-1|0|1, -1|0|1}.
 -type result() :: none| {max, Path :: [grid()]} | {{max_limited, Path :: [grid()]}}.
--type options() :: [{max_limit, non_neg_integer()}].
+-type option() :: {max_limit, non_neg_integer()} | {distance_function, manhattan|euclidean}.
+-type options() :: [option()].
 
 
--spec search(StartGrid, EndGrid, ValidFun, DirectionList, Options) -> Result when
+-spec search(StartGrid, EndGrid, ValidFun, Options) -> Result when
     StartGrid :: grid(), EndGrid :: grid(),
     ValidFun :: fun((CurGrid :: grid()) -> boolean()),
-    DirectionList :: [direction()],
     Options :: options(),
     Result :: result().
-search(StarGrid, EndGrid, ValidFun, DirectionList, Options) ->
-    HeuristicFun = heuristic_fun(StarGrid),
+search(StarGrid, EndGrid, ValidFun, Options) ->
+    DistanceFunction = proplists:get_value(distance_function, Options, ?DISTANCE_FUNCTION),
+    {HeuristicFun, DirectionList} = heuristic_fun(StarGrid, DistanceFunction),
     NeighbourFn = neighbour_fun(HeuristicFun, ValidFun, DirectionList),
-    OpenTrees = push(0, StarGrid, 0, [], gb_trees:empty()),
-    ClosedSets = #{},
+    OpenGrids = push(0, StarGrid, 0, [], gb_trees:empty()),
+    ClosedGrids = #{},
     MaxLimit = proplists:get_value(max_limit, Options, ?MAX_LIMIT),
-    continue(EndGrid, NeighbourFn, OpenTrees, ClosedSets, MaxLimit).
+    continue(EndGrid, NeighbourFn, OpenGrids, ClosedGrids, MaxLimit).
 
 %%=====================================================
 %% Internal Function
 %%=====================================================
-heuristic_fun({X1, Y1}) ->
-    fun({X2, Y2}) ->
-        X = erlang:abs(X1 - X2),
-        Y = erlang:abs(Y1 - Y2),
-        math:sqrt(X * X + Y * Y)
-    end.
+heuristic_fun({X1, Y1}, manhattan) ->
+    F =
+        fun({X2, Y2}) ->
+            erlang:abs(X1 - X2) + erlang:abs(Y1 - Y2)
+        end,
+    DirectionList = [{-1, 0}, {0, -1}, {1, 0}, {0, 1}],
+    {F, DirectionList};
+
+heuristic_fun({X1, Y1}, euclidean) ->
+    F =
+        fun({X2, Y2}) ->
+            X = X1 - X2,
+            Y = Y1 - Y2,
+            erlang:trunc(math:sqrt(X * X + Y * Y) * 10)
+        end,
+    DirectionList = [{-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}],
+    {F, DirectionList}.
 
 neighbour_fun(HeuristicFun, ValidFun, DirectionList) ->
-    fun({X, Y} = CGrid, G, Path, OpenTrees, ClosedSets) ->
+    fun({X, Y} = CGrid, G, Path, OpenGrids, ClosedGrids) ->
         Fun =
-            fun({XOffset, YOffset}, AccOpenTrees) ->
+            fun({XOffset, YOffset}, AccOpenGrids) ->
                 NGrid = {X + XOffset, Y + YOffset},
-                case is_close(NGrid, ClosedSets) of
+                case is_closed(NGrid, ClosedGrids) of
                     true ->
-                        AccOpenTrees;
+                        AccOpenGrids;
                     false ->
                         G1 = G + g(CGrid, NGrid),
                         NewScore = G1 + HeuristicFun(NGrid),
-                        case not is_open(NewScore, NGrid, AccOpenTrees)
+                        case not is_open(NewScore, NGrid, AccOpenGrids)
                             andalso ValidFun(NGrid) of
                             true ->
-                                push(NewScore, NGrid, G1, [NGrid | Path], AccOpenTrees);
+                                push(NewScore, NGrid, G1, [NGrid | Path], AccOpenGrids);
                             false ->
-                                AccOpenTrees
+                                AccOpenGrids
 
                         end
                 end
             end,
-        lists:foldl(Fun, OpenTrees, DirectionList)
+        lists:foldl(Fun, OpenGrids, DirectionList)
     end.
 
-%% G值开销
-g({X, _}, {X, _}) -> 1;
-g({_, Y}, {_, Y}) -> 1;
-g(_, _) -> 1.41421.
+g({X, _}, {X, _}) ->
+    10;
+g({_, Y}, {_, Y}) ->
+    10;
+g(_, _) ->
+    14.
 
-is_open(Score, Grid, OpenTrees) ->
-    gb_trees:is_defined({Score, Grid}, OpenTrees).
+is_open(Score, Grid, OpenGrids) ->
+    gb_trees:is_defined({Score, Grid}, OpenGrids).
 
-is_close(Grid, CloseSets) ->
-    case CloseSets of
+is_closed(Grid, ClosedGrids) ->
+    case ClosedGrids of
         #{Grid := _} ->
             true;
         #{} ->
             false
     end.
 
-push(Score, Grid, G, Path, OpenTrees) ->
-    io:format("push:~w~n", [{Score, Grid, G}]),
-    gb_trees:insert({Score, Grid}, {G, Path}, OpenTrees).
+push(Score, Grid, G, Path, OpenGrids) ->
+    gb_trees:insert({Score, Grid}, {G, Path}, OpenGrids).
 
-continue(EndGrid, NeighbourFun, OpenTrees, ClosedSets, MaxLimit) when MaxLimit > 0 ->
-    case pop_best(OpenTrees, ClosedSets) of
+continue(EndGrid, NeighbourFun, OpenGrids, ClosedGrids, MaxLimit) when MaxLimit > 0 ->
+    case pop_best(OpenGrids, ClosedGrids) of
         none ->
             none;
-        {EndGrid, _G, Path, _NewOpenTrees, _NewClosedSets} ->
+        {EndGrid, _G, Path, _NewOpenGrids, _NewClosedGrids} ->
             {max, Path};
-        {Grid, G, Path, NewOpenTrees, NewClosedSets} ->
-            OpenTrees2 = NeighbourFun(Grid, G, Path, NewOpenTrees, NewClosedSets),
-            continue(EndGrid, NeighbourFun, OpenTrees2, NewClosedSets, MaxLimit - 1)
+        {Grid, G, Path, NewOpenGrids, NewClosedGrids} ->
+            OpenGrids2 = NeighbourFun(Grid, G, Path, NewOpenGrids, NewClosedGrids),
+            continue(EndGrid, NeighbourFun, OpenGrids2, NewClosedGrids, MaxLimit - 1)
     end;
-continue(_EndGrid, _NeighbourFun, OpenTrees, ClosedSets, _MaxLimit) ->
-    {_Grid, _G, Path, _NewOpenTrees, _NewClosedSets} = pop_best(OpenTrees, ClosedSets),
-    {max_limited, Path}.
+continue(_EndGrid, _NeighbourFun, _OpenGrids, _ClosedGrids, _MaxLimit) ->
+    max_limited.
 
-pop_best(OpenTrees, ClosedSets) ->
-    case gb_trees:is_empty(OpenTrees) of
+pop_best(OpenGrids, ClosedGrids) ->
+    case gb_trees:is_empty(OpenGrids) of
         true ->
             none;
         false ->
-            {{_BestScore, Grid}, {G, Path}, NewOpenTree} = gb_trees:take_smallest(OpenTrees),
-            NewClosedSets = ClosedSets#{Grid => true},
-            io:format("pop_best:~w~n", [Grid]),
-            {Grid, G, Path, NewOpenTree, NewClosedSets}
+            {{_BestScore, Grid}, {G, Path}, NewOpenTree} = gb_trees:take_smallest(OpenGrids),
+            NewClosedGrids = ClosedGrids#{Grid => true},
+            {Grid, G, Path, NewOpenTree, NewClosedGrids}
     end.
 
 -ifdef(TEST).
@@ -110,10 +121,72 @@ pop_best(OpenTrees, ClosedSets) ->
 
 pathfinder() ->
     ?pathfinder_test([
-        "  S  ",
-        "XXXX ",
-        "E    "
-    ], []).
+        "                                                            ",
+        " S  XXX                                                     ",
+        "    X                                                       ",
+        " XXXX                                                       ",
+        "                                                            ",
+        "                                                            ",
+        "              XXXX                                          ",
+        "            XXXXXXXXX                                       ",
+        "         XXXXXXXX                                           ",
+        "      XXXXXXXX                           XX                 ",
+        "                                         XX                 ",
+        "                     X                   XX                 ",
+        "                    XXX                  XX                 ",
+        " XXXXXXXXXXXXX      XXX                  XX                 ",
+        "    XXXXXX           X                   XX                 ",
+        "      XX                                 XX                 ",
+        "                                         XX                 ",
+        "                                         XX                 ",
+        "                 XXXXXX                  XX                 ",
+        "                XXXXXXXXXXXX             XXX                ",
+        "                    XXXXXXX         XXXXXXXX                ",
+        "                                  XXXXXXXXXXX               ",
+        "                                 XXXXXXXXXXXXXX             ",
+        "                               XXXXXXXXXX                   ",
+        "                       XXXXXXXXXXXXXXXXXXX                  ",
+        "                               XXXXXXXXXXXXXX               ",
+        "                                          XXXXXXXX          ",
+        "                                                            ",
+        "                                                            ",
+        "                                                            ",
+        "                                         X                  ",
+        "                                         X                  ",
+        "                                        XX                  ",
+        "         XXX                         XXXX                   ",
+        "       XXXXXXXX                XXXXXXXX                     ",
+        "     XXXXXXXXXXX      XXXXXXXXXXXXXX                        ",
+        "  XXXXXXXXXXXXXXXXXXXXXXXXX                                 ",
+        "      XXXXXXXXXXXXXXX                                       ",
+        "                                                       X    ",
+        "                                                      XX    ",
+        "                                                    XXX     ",
+        "               XXXXXX                           XXXXXXX     ",
+        "                 XXX                XXXXXXXXXXXXXXXXX       ",
+        "                               XXXXXXXXXXXXXXXXXXX          ",
+        "                                                            ",
+        "                                                            ",
+        "                     XXXXXXXXXXXXXXX                        ",
+        "                 XXXXXXX        XXXX                        ",
+        "             XXXXXXX              XX                        ",
+        "         XXXXXXXXXXXXXX            X            XXXXXXXXXXXX",
+        "           XXXXXXXXXXXXXX                                   ",
+        "                                                            ",
+        "                                       XXXXXXXXXXXXX        ",
+        "                                      XXXXXXXXXXXXXXXXXXXX  ",
+        "                                     XXXXXXXXXXXXXXX        ",
+        "                                    XXXXXXXXXXXX            ",
+        "                                           XXX              ",
+        "                                            X               ",
+        "                                                          E ",
+        "                                                            "
+    ], [{distance_function, euclidean}]).
+%%    ?pathfinder_test([
+%%        "  S  ",
+%%        "XXXX ",
+%%        "E    "
+%%    ], []).
 
 run_pathfinder_test(WorldDiagram, Options) ->
     {StartGrid, EndGrid, World} = init_word(WorldDiagram),
@@ -124,10 +197,9 @@ run_pathfinder_test(WorldDiagram, Options) ->
             X =< MaxX andalso Y =< MaxY andalso 0 < X andalso 0 < Y
                 andalso element(Y, maps:get(X, World)) =/= $X
         end,
-    DirectionList = [{-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}],
-    case search(StartGrid, EndGrid, ValidFun, DirectionList, Options) of
-        {max_limited, Path} ->
-            io:format("worklimited path:~w~n", [Path]);
+    case search(StartGrid, EndGrid, ValidFun, Options) of
+        max_limited ->
+            io:format("worklimited~n", []);
         {max, Path} ->
             drw_world(Path, World);
         none ->
@@ -163,7 +235,8 @@ find_grid_col(_C, [], _Col) ->
     none.
 
 drw_world(Path, World) ->
-    Fun = fun({X, Y}, AccWorld) -> maps:update(X, setelement(Y, maps:get(X, AccWorld), $o), AccWorld) end,
+    Fun = fun({X, Y}, AccWorld) ->
+        maps:update(X, setelement(Y, maps:get(X, AccWorld), $o), AccWorld) end,
     NewWorld = lists:foldl(Fun, World, Path),
     drw_world_1(1, NewWorld).
 drw_world_1(Row, World) ->
